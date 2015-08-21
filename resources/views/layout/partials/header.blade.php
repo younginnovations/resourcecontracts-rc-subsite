ae43e13af94a48c2ba33928c7d3fce4e671f5e6c
Manish Gopal Singh <manish.singh@yipl.com.np>
2015-08-21 18:30:31 +0545

    updated header

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
         <div class="navbar-header">
             <span data-toggle="collapse-side" data-target=".sidebar-collapse" data-target-2=".sidebar-collapse-container" class="pull-left trigger">trigger</span>
-            <a class="navbar-brand" href="index.html" >Resource <span>Contracts</span></a>
+            <a class="navbar-brand" href="{{url()}}" >Resource <span>Contracts</span></a>
         </div>
         <div class="navbar-right">
             <form action="{{url('search')}}" method="get" class="search-form">
@@ -13,54 +13,4 @@
             </form>
         </div>
     </nav>
-</div>
-
-
-
-<div id="sidebar-wrapper" class="sidebar-collapse in">
-    <ul class="sidebar-nav">
-        <li class="sidebar-brand">
-            <a href="http://rc-site-demo.elasticbeanstalk.com/site/public">Resource <span>Contracts</span></a>
-        </li>
-        <li class="contracts active">
-            <a href="http://rc-site-demo.elasticbeanstalk.com/site/public">
-                <span>All Contracts</span>
-                <small class="label pull-right">6</small>
-            </a>
-        </li>
-        <li class="countries">
-            <label for="">Countries</label>
-            <ul>
-                <li>
-                    <a href="http://rc-site-demo.elasticbeanstalk.com/site/public/filter?country=as">
-                        <span>American samoa</span>
-                        <small class="label pull-right">2</small>
-                    </a>
-                </li>
-            </ul>
-
-        </li>
-        <li class="year">
-            <label for="">Year</label>
-            <ul>
-                <li>
-                    <a href="http://rc-site-demo.elasticbeanstalk.com/site/public/filter?year=2015">
-                        <span>2015</span>
-                        <small class="label pull-right">3</small>
-                    </a>
-                </li>
-            </ul>
-        </li>
-        <li class="resources">
-            <label for="">Resources</label>
-            <ul>
-                <li>
-                    <a href="http://rc-site-demo.elasticbeanstalk.com/site/public/filter?resource=base">
-                        <span>Base</span>
-                        <small class="label pull-right">3</small>
-                    </a>
-                </li>
-            </ul>
-        </li>
-    </ul>
 </div>
--- resources/views/site/filter.blade.php
+++ resources/views/site/filter.blade.php
@@ -1,27 +1,26 @@
 @extends('layout.app-full')
 
 @section('content')
-
-   @include('layout.partials.header')
-
     <div class="row">
         <div class="col-lg-12 panel-top-wrapper">
-            <div class="breadcrumb-wrapper">
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li>Search Contracts</li>
-                </ul>
+            <div class="panel-top-content">
+                <div class="pull-left">
+                    <div class="breadcrumb-wrapper">
+                        <ul>
+                            <li><a href="{{url()}}">Home</a></li>
+                            <li>Search</li>
+                        </ul>
+                    </div>
+                    <div class="panel-title">
+                        Search
+                    </div>
+                </div>
             </div>
-            <div class="panel-title">
-                Search Contracts
-            </div>
-
         </div>
     </div>
     <div class="row">
         <div class="col-lg-12 country-list-wrapper">
             <div class="panel panel-default panel-wrap country-list-wrap">
-                <div class="panel-heading">Contracts in Guinea</div>
                 <div class="panel-body">
                     <table class="table table-responsive table-contract">
                         <tbody>
@@ -51,9 +50,4 @@
             </div>
         </div>
     </div>
-
-
-
-
-
 @stop
