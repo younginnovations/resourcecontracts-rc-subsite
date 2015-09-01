970bb0c1fc2b6fccfcc92283bac58e056c63a005
deepak <deepak.shah@yipl.com.np>
2015-09-01 10:48:24 +0545

    open contract clickable

--- resources/views/contract/detail.blade.php
+++ resources/views/contract/detail.blade.php
@@ -22,7 +22,6 @@
     @stop
 
 @section('content')
-    {{_e($contract->metadata,'government_entity','-')}}
 <div class="row">
     <div class="col-lg-12 panel-top-wrapper">
         <div class="panel-top-content">
@@ -181,7 +180,7 @@
                 <ul>
                     <li>
                         <label for="">Open Corporate ID</label>
-                        <span>{{_e($company,'open_corporate_id','-')}}</span>
+                        <span>@if(isset($company->open_corporate_id) && !empty($company->open_corporate_id))<a href="{{$company->open_corporate_id}}">{{str_limit($company->open_corporate_id,100)}}</a> @else - @endif</span>
                     </li>
                     <li>
                         <label for="">Participation Share</label>
