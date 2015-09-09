ed1913d7be3bcd5c042b92f073d9158631b1182f
Manish Gopal Singh <manish.singh@yipl.com.np>
2015-09-09 10:46:08 +0545

    added database for cms

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,20 +1,31 @@
 <?php
 
+use App\Http\Models\Page\Page;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
-class DatabaseSeeder extends Seeder {
+class DatabaseSeeder extends Seeder
+{
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Model::unguard();
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
 
-		// $this->call('UserTableSeeder');
-	}
+        $pages = [
+            ['title' => 'About', 'slug' => 'about', 'content' => 'About content'],
+            ['title' => 'Contact', 'slug' => 'contact', 'content' => 'Contact content'],
+            ['title' => 'Guides', 'slug' => 'guides', 'content' => 'Guide content'],
+            ['title' => 'FAQs', 'slug' => 'faqs', 'content' => 'FAQs content'],
+        ];
+
+        foreach ($pages as $page) {
+            Page::create($page);
+        }
+    }
 
 }
