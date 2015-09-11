429576ea80385cd97205c95976379c301f864ddb
Sweta Shrestha <sweta.shrestha@yipl.com.np>
2015-09-11 15:31:43 +0545

    ui bug fixes

--- resources/views/errors/error.blade.php
+++ resources/views/errors/error.blade.php
@@ -3,7 +3,7 @@
 <div class="row">
     <div class="col-lg-12 not-found-wrapper">
         <div class="not-found-container">
-            <div class="not-found-content">
+            <div class="not-found-content service-not-found-content">
                 <h1><strong>Service Temporarily Unavailable</strong></h1>
 
                 <p style="font-size: 25px;width: 600px">The system is having some problems. Please contact administrator resourcecontracts@yipl.com.np if the problem persists.</p>
