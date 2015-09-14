dddfde96f7f756bf2e094bbce3413240a4792d54
Anjesh Tuladhar <anjeshtuladhar@gmail.com>
2015-09-14 16:57:13 +0545

    init-working-with-design

---
+++ public/scripts/contract.view.custom/views/annotations.view.js
@@ -0,0 +1,190 @@
+var AnnotationHeader = React.createClass({
+    componentDidMount: function() {
+        var self = this;
+        this.props.annotationsCollection.on("reset", function() {
+          self.forceUpdate();
+        });
+    },
+    render: function() {
+        var count = this.props.annotationsCollection.length;
+        return (
+            <div className="annotation-title">Annotations - {count}</div>
+        );
+    }
+});
+
+var AnnotationItem = React.createClass({
+    handleAnnotationClick: function(e) {
+        var self = this;
+        if(this.props.annotation.get('shapes')) {
+            this.props.contractApp.setView("pdf");
+            location.hash = "#/pdf";
+            setTimeout(self.props.contractApp.setCurrentPage(self.props.annotation.get('page_no')), 100);   
+            this.props.contractApp.triggerUpdatePdfPaginationPage(self.props.annotation.get('page_no'));
+            // this.props.currentPdfPage.set({"page_no": this.props.annotation.get('page_no')});
+            // annotation_type = "pdf";
+        } else if(this.props.annotation.get('ranges')) {
+            location.hash = "#/text";
+            this.props.contractApp.setView("text");
+            this.props.contractApp.setCurrentPage(this.props.annotation.get('page_no'));
+            setTimeout(this.props.contractApp.triggerScrollToTextPage());
+            // location.hash = "#/text/page/"+this.props.annotation.get('page_no');
+            // this.props.currentPage.set({"page_no": this.props.annotation.get('page_no')});
+            // this.props.currentPage.trigger("scroll-to-page");
+            // annotation_type = "text";
+        }
+    },
+    truncate: function(str, num){
+        num = num || 20;
+        str = str + '';
+        var words = str.split(' ');
+        if(num > words.length) {
+            return str;
+        } else {
+            this.props.annotation.ellipsis = "showmore";
+            words = words.splice(0,num);
+            return words.join(' ');
+        }
+    },
+    handleEllipsis: function(e) {
+        var text = e.target.innerHTML;
+        if(text == "...more") {            
+            this.props.annotation.ellipsis = "showless";
+        } else if(text == "...less") {
+            this.props.annotation.ellipsis = "showmore";
+        }
+        this.forceUpdate();
+    },
+    render: function() {
+        var category = this.props.annotation.get('category');
+        var id = this.props.annotation.get('id');
+        var text = this.props.annotation.get('text');
+        var quote = this.props.annotation.get('quote');
+        var page_no = "Pg " + this.props.annotation.get('page_no');
+        var annotation_type = "";
+        if(this.props.annotation.get('shapes')) {
+            annotation_type = "pdf";
+        } else if(this.props.annotation.get('ranges')) {
+            annotation_type = "text";
+        }
+        var showText = text,
+            ellipsistext = "";
+        if(!this.props.annotation.ellipsis || this.props.annotation.ellipsis == "showmore") {
+            showText = this.truncate(text);            
+        }
+        if(this.props.annotation.ellipsis == "showmore") {
+            ellipsistext = "...more";
+        } else if(this.props.annotation.ellipsis == "showless") {
+            ellipsistext = "...less";
+        }
+        var currentAnnotationClass = "";
+        if(this.props.contractApp.getSelectedAnnotation() === this.props.annotation.get('id')) {
+            currentAnnotationClass = "selected-annotation";
+            // this.props.contractApp.resetSelectedAnnotation();
+        }
+        
+        return (
+            <div className="annotation-category">
+                {category}
+                <ul>
+                    <li className={currentAnnotationClass}>
+                        <div id={id}>
+                            <span onClick={this.handleAnnotationClick}>{page_no}</span>
+                            <span onClick={this.handleAnnotationClick}> {showText} </span> 
+                            <a onClick={this.handleEllipsis}>{ellipsistext}</a>
+                            - {annotation_type}
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+});
+
+var AnnotationsSort = React.createClass({
+    getInitialState: function() {
+        return {
+            value: 'sort-by-page'
+        }
+    },    
+    handleSelect: function(e) {
+        this.setState({value: event.target.value});
+        if("sort-by-category" === event.target.value) {
+            annotationsCollection.setSortByKey("category");
+        } else {
+            //default sort by page
+            annotationsCollection.setSortByKey("page_no");
+        }
+        annotationsCollection.trigger("reset");
+    },
+    render: function() {
+        return (
+            <div className="annotation-sort">
+                Sort by 
+                <select id="annotation-sort-by" onChange={this.handleSelect} value={this.state.value}>
+                    <option value="sort-by-page">Page</option>
+                    <option value="sort-by-category">Category</option>
+                </select>
+            </div>
+        );        
+    }
+});
+
+var AnnotationsList = React.createClass({
+    componentDidMount: function() {
+        var self = this;
+        this.props.annotationsCollection.on("reset", function() {
+          self.forceUpdate();
+          if(self.props.contractApp.getSelectedAnnotation()) {
+            self.props.contractApp.triggerScrollInAnnotationList();
+          }
+        });
+        this.props.contractApp.on("scroll-in-annotation-list", function() {
+            self.scrollToAnnotation(self.props.contractApp.getSelectedAnnotation());
+        });
+    },
+    scrollToAnnotation: function(annotation_id) {
+        if(annotation_id) {
+            var pageOffsetTop = $('#'+annotation_id).offset().top - 70;
+            var parentTop = $('.annotations-viewer').scrollTop();
+            var parentOffsetTop = $('.annotations-viewer').offset().top
+            $('.annotations-viewer').animate({scrollTop: parentTop - parentOffsetTop + pageOffsetTop},500);
+            self.props.contractApp.resetSelectedAnnotation();            
+        }
+    },    
+    render: function() {
+        var annotationsList;
+        var self = this;
+        this.props.annotationsCollection.sort();
+        if(this.props.annotationsCollection.models.length > 0) {
+          annotationsList = this.props.annotationsCollection.models.map(function(model, i) {
+            return (
+              <AnnotationItem 
+                key={i}
+                contractApp={self.props.contractApp} 
+                annotation={model} />
+            );
+          });
+        }
+        return (
+          <div className="annotations-list">
+          {annotationsList}
+          </div>
+        );
+    }
+});
+
+
+var AnnotationsViewer = React.createClass({
+    render: function() {
+        return(
+            <div className="annotations-viewer" style={this.props.style}>
+                <AnnotationHeader annotationsCollection={this.props.annotationsCollection} />
+                <AnnotationsSort annotationsCollection={this.props.annotationsCollection} />
+                <AnnotationsList 
+                    contractApp={this.props.contractApp} 
+                    annotationsCollection={this.props.annotationsCollection} />
+            </div>
+        );
+    }
+});
---
+++ public/scripts/contract.view.custom/views/text.view.js
@@ -0,0 +1,154 @@
+var NavigationView = React.createClass({
+  render: function() {
+    return (
+      <div className="navigation">
+        <a href="#/text">Text</a>
+        <a href="#/pdf">Pdf</a>        
+      </div>
+    );
+  }
+});
+//<a href="#/both">Both</a>
+var TextPaginationView = React.createClass({
+  getInitialState: function() {
+    return {
+      visiblePage: 1,
+      totalPages: 0
+    }
+  },
+  changePage: function(page_no) {
+    this.refs.userInputText.getDOMNode().value = page_no;
+    this.props.contractApp.setCurrentPage(page_no);
+    this.setState({visiblePage: page_no});
+    this.props.contractApp.triggerScrollToTextPage();
+    // this.props.contractApp.trigger("scroll-to-page");
+    // this.props.currentPage.set({"page_no": page_no});  
+    // this.props.currentPage.trigger("scroll-to-page");
+  },
+  clickPrevious: function(e) {
+    e.preventDefault();
+    if(this.state.visiblePage > 1) {
+      this.changePage(this.state.visiblePage-1);
+    }
+  },
+  clickNext: function(e) {
+    e.preventDefault();
+    if(this.state.visiblePage < this.state.totalPages) {
+      this.changePage(this.state.visiblePage+1);
+    }
+  },
+  handleKeyDown: function(e) {
+    if(e.keyCode == 13) {
+      var inputPage = parseInt(this.refs.userInputText.getDOMNode().value);
+      if(inputPage > 0 && inputPage <= this.state.totalPages) {
+        this.changePage(inputPage);
+      } else {
+        this.changePage(this.state.visiblePage);
+      }     
+    }
+  },
+  componentDidMount: function() {
+    var self = this;
+    self.setState({totalPages: self.props.contractApp.getTotalPages()});
+    this.props.contractApp.on("update-text-pagination-page", function(page_no) {
+      self.refs.userInputText.getDOMNode().value = page_no;
+      self.setState({visiblePage: page_no});
+    });
+    // this.props.currentPage.on("update-pagination-page", function(page_no) {
+    //   self.refs.userInputText.getDOMNode().value = page_no;
+    //   self.setState({visiblePage: page_no});
+    // });
+    // this.props.pagesCollection.on("reset", function() {
+    //   self.setState({totalPages: self.props.pagesCollection.length});
+    // });
+    this.refs.userInputText.getDOMNode().value = this.state.visiblePage;
+  },  
+  render: function() {
+    return (
+      <div className="text-pagination pagination" style={this.props.style}>
+        <a href="#" className="previous" onClick={this.clickPrevious}>Previous</a>
+        <input type="text" className="goto" ref="userInputText" onKeyDown={this.handleKeyDown} />
+        <a href="#" className="next" onClick={this.clickNext}>Next</a> of {this.state.totalPages}
+      </div>
+    );
+  }
+});
+
+var TextPageView = React.createClass({
+  _onEnter: function(msg, e) {
+    this.props.contractApp.triggerUpdateTextPaginationPage(this.props.page.get("page_no"));
+  },
+  _onLeave: function(e) {
+  },
+  render: function() {
+    var text = this.props.page.get('text');
+    var page_no = this.props.page.get('page_no');
+    return (      
+      <span className={page_no} >
+        <span>{page_no}</span>
+        <span dangerouslySetInnerHTML={{__html: text}} />
+        <Waypoint
+            onEnter={this._onEnter.bind(this, "enter" + page_no)}
+            onLeave={this._onLeave}
+            threshold={-0.4}/> 
+      </span>
+    );
+  }
+});
+var TextViewer = React.createClass({
+  getInitialState: function() {
+    return {content: "Please wait while loading ..."};
+  },
+  loadAnnotations: function() {
+    if(!this.annotator) {
+      this.annotator = new AnnotatorjsView({
+          el: ".text-annotator",
+          api: this.props.contractApp.getLoadAnnotationsUrl(),
+          availableTags: ["Country","Local-Company-Name"],
+          // collection: annotationCollection,
+          annotationCategories: ["General information","Country","Local company name"],
+          enablePdfAnnotation: false,
+          contractApp: this.props.contractApp
+      });
+    }
+  },
+  scrollToPage: function(page) {
+    var pageOffsetTop = $('.'+page).offset().top;
+    var parentTop = $('.text-annotator ').scrollTop();
+    var parentOffsetTop = $('.text-annotator').offset().top
+    $('.text-annotator').animate({scrollTop: parentTop - parentOffsetTop + pageOffsetTop},100);
+  },
+  componentDidMount: function() {
+    var self = this;
+    this.props.pagesCollection.on("reset", function() {
+      self.forceUpdate();
+      self.loadAnnotations();
+      self.props.contractApp.triggerScrollToTextPage();
+    });
+    this.props.contractApp.on("scroll-to-text-page", function() {
+      self.scrollToPage(self.props.contractApp.getCurrentPage());
+    });
+  },
+  render: function() {
+    var self = this;
+    var pagesView = "Please wait while loading ...";
+    if(this.props.pagesCollection.models.length > 0) {
+      pagesView = this.props.pagesCollection.models.map(function(model, i) {
+        return (
+          <TextPageView
+            key={i} 
+            contractApp={self.props.contractApp} 
+            page={model} />
+        );
+      });
+    }
+    return (
+      <div className="text-annotator" style={this.props.style} >
+        <div></div>
+        <div className="text-viewer">
+        {pagesView}
+        </div>
+      </div>
+    );
+  }
+});
