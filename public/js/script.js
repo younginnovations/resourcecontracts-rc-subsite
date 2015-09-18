b7d262bdebd284d90659a8ca51a0254b7c4812ce
Manoj Byanjankar <manoj.byanjankar@yipl.com.np>
2015-09-18 11:11:19 +0545

    Sprint 8 minor fixes
     annotation category clickable detail page
    
     sort company,annotation category filter list
    
     olc homepage background image changed

--- public/js/script.js
+++ public/js/script.js
@@ -74,6 +74,25 @@ $(document).ready(function () {
 
     });
 
+    // slide to annotation list
+
+    $(".view-annotation-category").on('click', function (e) {
+
+        // prevent default anchor click behavior
+        e.preventDefault();
+
+        // animate
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 300, function () {
+
+            // when done, add hash to url
+            // (default click behaviour)
+            window.location.hash = this.hash;
+        });
+
+    });
+
     // search form toggle
 
     $('#search-form:not(.search-page-form) input[type="text"]').focus(function () {
