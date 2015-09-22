a4bfab2e76b94cd221f42d7c2d2d98dcfb1fc7c8
Manish Gopal Singh <manish.singh@yipl.com.np>
2015-09-22 16:23:14 +0545

    added compare checkbox

--- resources/views/site/filter.blade.php
+++ resources/views/site/filter.blade.php
@@ -1,7 +1,6 @@
 @extends('layout.app-full')
 
 @section('content')
-
     <div class="row">
         <div class="col-lg-12 panel-top-wrapper search-top-wrapper">
             <div class="panel-top-content">
@@ -31,17 +30,24 @@
         <div class="col-lg-12 country-list-wrapper search-list-wrapper">
             <div class="panel panel-default panel-wrap country-list-wrap">
                 <div class="panel-body">
+                    <div id="compare-block">
+                        <h2>Compare</h2>
+                        <ul>
+                        </ul>
+                        <button>Compare</button>
+                    </div>
+
                     <table class="table table-responsive table-contract">
                         <tbody>
                         @forelse($contracts->results as $contract)
                             <?php
                             $api = app('App\Http\Services\APIService');
                             $annotations = $api->getAnnotations($contract->contract_id);
-
                             ?>
                             <tr>
                                 <td>
-                                    <a href="{{route('contract.detail',['id'=>$contract->contract_id ])}}">
+                                    <input type="checkbox" class="compare" name="compare[]" value="{{$contract->contract_id}}" />
+                                    <a class="title-{{$contract->contract_id}}" href="{{route('contract.detail',['id'=>$contract->contract_id ])}}">
                                         {{ $contract->contract_name or ''}}
                                     </a>
                                     @if($annotations->total>0)
@@ -118,5 +124,187 @@
             </div>
         </div>
     </div>
+@stop
 
+@section('js')
+    <script src="{{url('js/jquery.cookie.js')}}"></script>
+    <script>
+       $(function(){
+           var compareList = function(cookieName) {
+               return {
+                   "add": function(val) {
+                       var items = this.items();
+                       if(items)
+                       {
+                           items.push(val);
+                       }
+                       else
+                       {
+                           items = [val];
+                       }
+                       Cookies.set(cookieName, JSON.stringify(items));
+                   },
+                   "remove": function (index) {
+                       var items = this.items();
+                           items.splice(index, 1);
+                       Cookies.set(cookieName, JSON.stringify(items));
+                   },
+                   "clear": function() {
+                       Cookies.remove(cookieName);
+                   },
+                   "items": function() {
+                       return Cookies.getJSON(cookieName);
+                   },
+                   "isLimitExceed":function(){
+                       return this.items().length > 1;
+                   },
+                   "delete":function(id){
+                       console.log(id);
+                       var self = this;
+                       $.each(this.items(), function(index,val){
+                           if(val.id == id)
+                           {
+                               self.remove(index);
+                           }
+                       });
+                   }
+               }
+           };
+
+           var compareView = function(){
+               return {
+                    "disableCheckbox" :function(el)
+                    {
+                        $(el).attr('disabled','disabled');
+                    },
+                   "enableCheckbox" :function(el)
+                   {
+                       $(el).removeAttr('disabled');
+                   },
+                   "itemTemplate" : function (id, title) {
+                       return '<li class="item-'+id+'">'+title+' <a href="#" data-id="'+id+'" class="item-delete">delete</a></li>';
+                   },
+                   "removeItem":function(id)
+                   {
+                       $('.item-'+id).remove();
+                   },
+                   "render":function(el, items)
+                   {
+                       if(items.length > 0)
+                       {
+                           $('#compare-block').show();
+                       }else {
+                           $('#compare-block').hide();
+                       }
+
+                       $(el).html('');
+
+                       var self = this;
+
+                       $.each(items, function(index, val){
+
+                           $(el).append(self.itemTemplate(val.id,val.title));
+                       });
+                   }
+               };
+           };
+
+           var list = new compareList('contractList');
+           var listView = new compareView('compareView');
+           init();
+           $(document).on('change','.compare', function(){
+               var id = $(this).val();
+               var checked = this.checked;
+               if(checked)
+               {
+                   var title=$('.title-'+id).text();
+                   if(!list.isLimitExceed())
+                   {
+                       list.add({"id":id,"title": title});
+                   }
+                   listView.render('#compare-block ul', list.items())
+               }else{
+                   list.delete(id);
+                   $("input[value="+id+"]").prop('checked', false);
+                   listView.render('#compare-block ul', list.items())
+               }
+
+               if(list.isLimitExceed()){
+                   listView.disableCheckbox('.compare');
+               }else{
+                   listView.enableCheckbox('.compare');
+
+               }
+
+               if(list.items().length >1)
+               {
+                   $('#compare-block button').show();
+               }else{
+                   $('#compare-block button').hide();
+               }
+
+               console.log(list.items());
+           });
+
+           $(document).on('click','.item-delete', function(e) {
+               e.preventDefault();
+               var id = $(this).data('id');
+               list.delete(id);
+               listView.removeItem(id);
+               listView.render('#compare-block ul', list.items());
+               if(list.isLimitExceed()){
+                   listView.disableCheckbox('.compare');
+               }else{
+                   listView.enableCheckbox('.compare');
+
+               }
+
+               if(list.items().length >1)
+               {
+                   $('#compare-block button').show();
+               }else{
+                   $('#compare-block button').hide();
+               }
+
+               $("input[value="+id+"]").prop('checked', false);
+
+           });
+
+           function init()
+           {
+               listView.render('#compare-block ul', list.items());
+
+               if(list.isLimitExceed()){
+                   listView.disableCheckbox('.compare');
+               }else{
+                   listView.enableCheckbox('.compare');
+               }
+
+               $.each(list.items(), function(index, value){
+                    $("input[value="+value.id+"]").prop('checked', true);
+               });
+
+               if(list.items().length >1)
+               {
+                   $('#compare-block button').show();
+               }else{
+                   $('#compare-block button').hide();
+               }
+
+               console.log(list.items());
+
+           }
+
+           $(document).on('click','#compare-block button', function(){
+               var url = '{{url('contract')}}';
+               $.each(list.items(), function(index, value){
+                   url+='/'+value.id;
+               });
+              url+='/compare';
+               window.location.href = url;
+
+           });
+
+       });
+    </script>
 @stop
