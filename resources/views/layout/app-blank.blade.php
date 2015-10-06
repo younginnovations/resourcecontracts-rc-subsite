174553038acdfe6b4416288340aac593d99351b5
Manish Gopal Singh <manish.singh@yipl.com.np>
2015-10-06 13:57:29 +0545

    added footer updated back button

--- resources/views/layout/app-blank.blade.php
+++ resources/views/layout/app-blank.blade.php
@@ -2,31 +2,5 @@
 
 @yield('content')
 
+@include('layout.partials.footer')
 
-
-    </div>
-</div>
-<?php
-$js = [
-        'jquery.min.js',
-        'bootstrap.min.js',
-        'metisMenu.min.js',
-        'raphael-min.js',
-        'script.js',
-        'select2.min.js',
-       // 'morris.min.js',
-     //   'morris-data.js',
-        'sb-admin-2.js'
-];
-?>
-@foreach($js as $link)
-    <script src="{{url(sprintf('js/%s',$link))}}"></script>
-@endforeach
-
-
-
-
-@yield('js')
-
-</body>
-</html>
