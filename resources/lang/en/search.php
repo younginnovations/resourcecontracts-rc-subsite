242fc9a1de1e0743c5c7b6216cb79e43f536165c
bijaya.kuikel <bijaya.kuikel@yipl.com.np>
2015-12-11 12:07:31 +0545

    Fixed the space of the contract view page
    * fixed the issue of link to all contracts page when 0 contracts is found while searching. (used named route)
    
    * changes from comments

--- resources/lang/en/search.php
+++ resources/lang/en/search.php
@@ -1,5 +1,6 @@
 <?php
 
+$link = route('contracts');
 return [
 
     'search_placeholder'   => 'Search for contracts...',
@@ -8,7 +9,7 @@ return [
     'contract_type'        => 'Contract Type',
     'annotations_category' => 'Annotations Category',
     'download_as_csv'      => 'Download search results as csv',
-    'search_not_found'     => "Your search has 0 results. To start a new search, enter your text in the search bar above. You can also <a href='/contracts'>browse a full list of available contracts. </a>",
+    'search_not_found'     => "Your search has 0 results. To start a new search, enter your text in the search bar above. You can also <a href=:link> browse a full list of available contracts. </a>",
     'corporate_group'      => 'Corporate Group',
     'found_in'             => 'Found In',
 
