a00e3d741a0be1949a26707d63275777609735d6
bijaya.kuikel <bijaya.kuikel@yipl.com.np>
2016-03-02 10:10:13 +0545

    Added responsive feature to popover upon hovering

--- resources/assets/scripts/script.js
+++ resources/assets/scripts/script.js
@@ -238,7 +238,31 @@ $(document).ready(function () {
             })
         }
     };
-    $('body').popover({selector: '[data-popover]', trigger: 'click hover', placement: 'auto', delay: {show: 50, hide: 400}});
+    var options = {
+        placement: function (context, source) {
+            var position = $(source).position();
 
+            if (position.left > 515) {
+                return "left";
+            }
+
+            if (position.left < 515) {
+                return "right";
+            }
+
+            if (position.top < 110) {
+                return "bottom";
+            }
+
+            return "top";
+        },
+        trigger: "click hover",
+        delay: {
+            show: 50,
+            hide: 400
+        }
+
+    };
+    $(".annotate-text").popover(options);
 
 });
