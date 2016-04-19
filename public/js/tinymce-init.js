c1f6a1ac34fd8b250abfede93d2dd7a6cdd223a2
upasana.ghimire <gupashana@gmail.com>
2016-04-19 11:24:36 +0545

    font-size-included
    font-size-included

--- public/js/tinymce-init.js
+++ public/js/tinymce-init.js
@@ -11,5 +11,5 @@ tinymce.init({
     "save table directionality template paste textcolor colorpicker"
   ],
   content_css: "css/content.css",
-  toolbar: "insertfile undo redo | styleselect | fontsizeselect| fontselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image |  media fullpage | forecolor backcolor",
+  toolbar: "insertfile undo redo | styleselect| fontselect | fontsizeselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image |  media fullpage | forecolor backcolor",
 });
