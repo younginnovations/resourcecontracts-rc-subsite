0d0ba9b69a42405690bc208d4ba8715a127319e5
Radhika <radhika.prajapati@yipl.com.np>
2016-06-30 14:02:25 +0545

    contract-popup-view theming of contract popup

--- resources/assets/scripts/popup/Title.js
+++ resources/assets/scripts/popup/Title.js
@@ -10,13 +10,14 @@ class Title extends Component {
     render() {
         return (
             <nav className="navbar navbar-static-top no--search--wrap">
-                    <div className="navbar-header">
-                        <a className="navbar-brand" href={APP_URL} >{this.props.site_name}<span className="beta">Beta</span><span>Contracts</span></a>
-                    </div>
+                <div className="navbar-header">
+                <span data-toggle="collapse-sidebar" data-target=".sidebar-collapse" data-target-2=".sidebar-collapse-container" className="pull-left trigger">trigger</span>
+                    <a className="navbar-brand" href={APP_URL} >{this.props.site_name}<span className="beta">Beta</span><span>Contracts</span></a>
+                </div>
 
-            <div className="navbar-right">
-                <span>{this.props.title}</span>
-            </div>
+                <div className="col-sm-12 col-md-9 col-lg-10 navbar-right">
+                    <span className="title">{this.props.title}</span>
+                </div>
             </nav>
         );
     }
