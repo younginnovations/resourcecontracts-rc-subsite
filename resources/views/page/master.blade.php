e008ddfe4a67007a9209021f3dc82f7e0914df82
Radhika <radhika.prajapati@yipl.com.np>
2016-09-01 10:05:39 +0545

    static-page-ui ui correction of about and glossary page

--- resources/views/page/master.blade.php
+++ resources/views/page/master.blade.php
@@ -4,7 +4,7 @@
         <div class="edit-mode"><div>@lang('admin.editing') <a target="_blank" href="{{route('admin.page.edit', ['id'=>$page->id])}}">@lang('admin.click_here')</a> @lang('admin.to_edit') - <a href="{{url('logout')}}">@lang('admin.logout')</a></div></div>
     @endif
 
-    <div class="content-wrap">
+    <div class="static__content-wrap">
         <h1 id="title">{{$page->title()}}</h1>
         <div id="content">{!!$page->content()!!}</div>
     </div>
