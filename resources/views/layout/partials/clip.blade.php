520e226835252bc83de1143fbb397180e83c53c2
Saugat Maharjan <saugat@Saugats-iMac.local>
2016-12-27 11:10:12 +0545

    made learn more link dynamic

--- resources/views/layout/partials/clip.blade.php
+++ resources/views/layout/partials/clip.blade.php
@@ -1,6 +1,6 @@
 @if(site()->isClipEnabled())
     <div class="clip-head">
-        <a href="{{route('clip.index')}}" id="annotation-count" data-toggle="popover"  data-title=" @lang('clip.view-all-clips') @lang('clip.or') <a href='https://www.google.com'>@lang('clip.learn_more')</a>" style="display: none"></a>
+        <a href="{{route('clip.index')}}" id="annotation-count" data-toggle="popover"  data-title=" @lang('clip.view-all-clips') ( <a target='_blank' href={{url('faqs')}}>@lang('clip.learn_more')</a>)" style="display: none"></a>
         <a href="#" class="" id="hide-annotation" style="display: none">@lang('clip.hide')</a>
     </div>
 @endif
