77d57e93ce6c56b3043290315f12f724b4c0e46e
Radhika <radhika.prajapati@yipl.com.np>
2017-01-16 10:37:46 +0545

    changed All Contracts to All Documents in sidebar

--- resources/lang/ar/sidebar.php
+++ resources/lang/ar/sidebar.php
@@ -2,6 +2,7 @@
 return [
     'home'             => 'الصفحة الرئيسية',
     'all_contracts'    => "جميع العقود",
+    'all_documents'    => "جميع المستندات",
     'view_by_country'  => 'العرض حسب الدولة',
     'view_by_resource' => 'العرض حسب المورد',
     'glossary'         => 'مسرد المصطلحات ',
--- resources/lang/fr/sidebar.php
+++ resources/lang/fr/sidebar.php
@@ -2,6 +2,7 @@
 return [
     'home'                     => 'Accueil',
     'all_contracts'            => "Tout les contrats ",
+    'all_documents'            => "Tous les documents",
     'view_by_country'          => 'Voir par pays',
     'view_by_resource'         => 'Voir par Resource',
     'glossary'                 => 'Glossaire',
