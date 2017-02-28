2c5e2bd12738b287eae0fdd000695b5b9f651b96
Ionut Prunache <ionut@vitaminsoftware.com>
2017-02-28 23:24:31 +0200

    #1057 List all possible server names for subsites nginx config

--- config/init.sh
+++ config/init.sh
@@ -2,8 +2,9 @@
 
 #IMPORTANT!: Note that we do envsubst for all env variables for env.template. Take care with '$' characters in env.template, strings that start with $ will be treated like env vars.
 
+# When defining server names put the most likely name first to optimize matching performance
 #rc
-export SERVER_NAME=${DOMAIN_PREFIX}resourcecontracts.org
+export SERVER_NAME="www.resourcecontracts.org resourcecontracts.org *.resourcecontracts.org"
 export CATEGORY=rc
 export TRACKING_ID=${RC_TRACKING_ID}
 export DB_DATABASE=${RC_DB_DATABASE}
@@ -14,7 +15,7 @@ envsubst '$SERVER_NAME $CATEGORY'< ./nginx_subsite.template > /etc/nginx/sites-e
 envsubst < ./env.template > /var/www/rc/.env
 
 #olc
-export SERVER_NAME=${DOMAIN_PREFIX}openlandcontracts.org
+export SERVER_NAME="www.openlandcontracts.org openlandcontracts.org www.openlandcontracts.com openlandcontracts.com *.openlandcontracts.org "
 export CATEGORY=olc
 export TRACKING_ID=${OLC_TRACKING_ID}
 export DB_DATABASE=${OLC_DB_DATABASE}
@@ -25,7 +26,7 @@ envsubst '$SERVER_NAME $CATEGORY'< ./nginx_subsite.template > /etc/nginx/sites-e
 envsubst < ./env.template > /var/www/olc/.env
 
 #country-tn
-export SERVER_NAME=${DOMAIN_PREFIX}tunisia.resourcecontracts.org
+export SERVER_NAME="tunisia.resourcecontracts.org *.tunisia.resourcecontracts.org"
 export TRACKING_ID=${TN_TRACKING_ID}
 export DB_DATABASE=${COUNTRY_DB_DATABASE}
 export CONTACT_MAIL=${TN_CONTACT_MAIL}
