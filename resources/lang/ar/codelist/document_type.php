73893eda6ca188d87052d38c58220674305f418b
Anjesh Tuladhar <anjesh@yipl.com.np>
2018-09-10 11:53:29 +0545

    doctype updated as per https://github.com/NRGI/resourcecontracts.org/issues/1073

--- resources/lang/ar/codelist/document_type.php
+++ resources/lang/ar/codelist/document_type.php
@@ -6,8 +6,12 @@
 |
 */
 return [
+    "Company-Company Contract"        => "عقد خاص",
+    "Company-State Contract"          => "عقد عام",
+    "Company-State Model Contract"    => "عقد عام نموذج عقد",
     "Contract"                        => "عقد",
-    "Environmental Impact Assessment" => "تقييم الأثر البيئي",
+    "Document Summary"                => "ملخص",
+    "Environmental and Social Document"=> "وثيقة ذات طابع بيئي و اجتماعي",
     "Feasibility Study"               => "دراسة الجدوى",
     "Financial information"           => "معلومات مالية",
     "Forest Management Plan"          => "خطة إدارة الغابات",
--- resources/lang/fr/codelist/document_type.php
+++ resources/lang/fr/codelist/document_type.php
@@ -6,14 +6,18 @@
 |
 */
 return [
+    "Company-Company Contract"        => "Contrat Privé",
+    "Company-State Contract"          => "Contrat Public",
+    "Company-State Model Contract"    => "Contrat Public Modèle de Contrat",
     "Contract"                        => "Contrat",
-    "Environmental Impact Assessment" => "Étude d'impact sur l'environnement",
+    "Document Summary"                => "Résumé",
+    "Environmental and Social Document"=> "Document Social et/ou Environmental",
     "Feasibility Study"               => "Étude de faisabilité",
     "Financial information"           => "Information financière",
     "Forest Management Plan"          => "Plan de gestion forestière",
     "Model Contract"                  => "Contrat-type",
-    "Other"                           => "Autres",
     "Notice"                          => "Préavis",
+    "Other"                           => "Autres",
     "Project Plan"                    => "Plan de projet",
     "Social Agreement"                => "Accord social",
     "Social Impact Assessment"        => "Étude d'impact sur la société",
