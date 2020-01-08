8e6dfb549ca99c2015a36175344cba75ab596c05
Bikram Tuladhar <bikramtuladhar2011@gmail.com>
2020-01-08 15:16:01 +0545

    1227-hide-search-bar-in-mobile-view  * [x] refactor hide search bar logic with blade template using section directive

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -2,22 +2,28 @@
 @include('layout.partials.clip')
 <div class="row title-wrap clearfix">
 	<nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
-		<?php
-		$segment = \Illuminate\Support\Facades\Request::segment(1);
-		$isGsearch = in_array($segment, ['gsearch']) ? true : false;
-		?>
+        <?php
+        $segment = \Illuminate\Support\Facades\Request::segment(1);
+        $isGsearch = in_array($segment, ['gsearch']) ? true : false;
+        $isSearchPage = in_array($segment, ['search']) ? true : false;
+        $api = app('App\Http\Services\APIService');
+        $summary = $api->sortSummaryCountry();
+        $attributes = $api->searchAttributed();
+        $category = $api->getAnnotationsCategory();
+        ?>
 		<form action="{{$isGsearch ? url('gsearch'): url('search')}}" method="get" class="search-form" id="search-form">
 			<div class="navbar-header">
 				<span data-toggle="collapse-sidebar" data-target=".sidebar-collapse"
 					  data-target-2=".sidebar-collapse-container" class="pull-left trigger"></span>
 				@include('layout.partials.logo')
 			</div>
-			<?php
-			$isSearchPage = in_array($segment, ['search']) ? true : false;
-			?>
-			<div class="right-header-section navbar-right hidden" @if(isset($hideSearchBar) && $hideSearchBar == true) style="display: none;" @endif>
-				@include('layout.partials.searchdropdown',["isSearchPage" => $isSearchPage])
-			</div>
+			@section('search-bar')
+				@if(!(isset($hideSearchBar) && $hideSearchBar == true))
+					<div class="right-header-section navbar-right hidden">
+						@include('layout.partials.searchdropdown',["isSearchPage" => $isSearchPage])
+					</div>
+				@endif
+			@show
 			@if(!$isSearchPage)
 				@include('layout.partials.search')
 				</form>
