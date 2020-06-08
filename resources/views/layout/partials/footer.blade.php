4d386325a5543179dfea2df2e597eaf47acb699c
Birendra Gurung <birendragurung007@gmail.com>
2020-06-08 14:16:17 +0545

    1289 Highlight and zoom the annotation 1290 Add a new URL column in the clips download file 1304 Add predefined text to clause summary 1296 Change Annotation category term in RC sites
    - [x] Added annotation page link to XLS download file
    - [x] Code modified for adding hyperlinks and bold header row
    - [x] changes in hyperlinks styles
    - [x] works on popup annotations
    - [x] open popup on page load
    - [x] send only one annotation to popup page
    - [x] Uncomment on Loading component
    - [x] refactoring code in Contract.js
    - [x] removed unused js file
    - [x] uncommenting changes
    - [x] render only single annotation highlights on pdf
    - [x] Fixed popup styles
    - [x] changed link on clips page
    - [x] Highlighting the annotation with zoom 1.5
    - [x] Refactoring code changes
    - [x] removing debugger
    - [x] Compiling js scripts
    - [x] Works on column renaming
    - [x] Works on translations
    - [x] Popup view button ui fix
    - [x] Display clip count if present on annotations page
    - [x] Resizing resource column width on clips page
    - [x] Show clip switch and count when clips are present in user's session
    - [x] Changed app version
    - [x] Composed AWS_URL from AWS env parameters
    - [x] Changed download filename to 'Clipped clauses DATE'
    - [x] Updated version for style.css
    - [x] Add predefined text when no annotation text provided on pdf page
    - [x] Changed download file-names for clipped items
    - [x] Added predefined text when no annotation text provided in clip page
    - [x] js builds
    - [x] Translations for 'clause summary not prepared'
    - [x] Renamed labels in search fields for RC sites to key clauses & tagged contracts only
    - [x] Updated style.css version to 1.09
    - [x] Added versions for js and css asset urls.
    - [x] Renamed zip filename for download
    - [x] Fixed translation issue on pdf viewer
    - [x] Create ExcelHelperTrait for
    - [x] Manage predefined text on xls file w.r.t rc/olc
    - [x] Changes made to excel creator on zip download process
    - [x] Cleanup excel file code
    - [x] Display annotation terms based on the site is RC or not
    - [x] Changes made to doc url
    - [x] Added PHP DocBlocks
    - [x] Minor fixes for translations
    - [x] Bugfix: removed unnecessary loop
    - [x] Save download file to downloads folder

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -19,7 +19,7 @@
 
 @include('contract.partials.emailModal')
 
-<script src="{{url('js/app.js')}}?v=2019-11-25"></script>
+<script src="{{url('js/app.js')}}?v=2020-07-22"></script>
 @yield('js')
 @stack('footer-scripts')
 @if(site()->hasTracking())
