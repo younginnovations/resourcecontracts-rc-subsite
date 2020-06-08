cbab444b6a78df78dcf7ec051c4255aaf0b6a034
Birendra Gurung <birendragurung007@gmail.com>
2020-06-08 14:16:17 +0545

    Added annotation page link to XLS download file
    Code modified for adding hyperlinks and bold header row
    
    changes in hyperlinks styles
    
    - works on popup annotations
    - open popup on page load
    - send only one annotation to popup page
    
    Uncomment on Loading component
    
    refactoring code in Contract.js
    
    removed unused js file
    
    uncommenting changes
    
    render only single annotation highlights on pdf
    
    Fixed popup styles
    
    changed link on clips page
    
    Highlighting the annotation with zoom 1.5
    
    Refactoring code changes
    
    removing debugger
    
    Compiling js scripts

---
+++ resources/assets/scripts/popup/config.js
@@ -0,0 +1,20 @@
+var Config = config;
+
+Config.facebookUrl = ()=> {
+    return config.share.facebook + encodeURIComponent(window.location.href);
+};
+Config.twitterUrl = ()=> {
+    return config.share.twitter;
+};
+
+Config.googleUrl = ()=> {
+    return config.share.google + encodeURIComponent(window.location.href);
+};
+
+Config.appUrl = (uri)=> {
+    return config.APP_URL + '/' + uri.trim('/');
+};
+
+Config.popupAnnotation = Annotation;
+
+export default Config;
