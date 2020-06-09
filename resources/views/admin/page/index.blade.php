91732dc7b95401fee325517a36b552dccd92215a
Birendra Gurung <birendragurung007@gmail.com>
2020-06-09 14:41:04 +0545

    ui changes

--- resources/views/admin/page/index.blade.php
+++ resources/views/admin/page/index.blade.php
@@ -19,8 +19,8 @@
 					<th>@lang('admin.id')</th>
 					<th>@lang('admin.page_title')</th>
 					<th>@lang('admin.slug')</th>
-					<th>@lang('admin.select_version')</th>
-					<th>@lang('admin.action')</th>
+					<th style="min-width: 178px;">@lang('admin.select_version')</th>
+					<th style="min-width: 228px;">@lang('admin.action')</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -35,7 +35,7 @@
 					<td>{{$page->slug}}</td>
 					<td>
 						@if($versions)
-							<span class="badge">v{{ $page->version_no }}</span>
+							<span class="badge" style="background-color: #337ab7;">v{{ $page->version_no }}</span>
 						@if (count($versions) > 1)
 								<a href="#" data-toggle="collapse" data-target=".page-version-collapsible-{{$page->id}}" class="see-versions" data-version-count="{{ count((array) $page->version) }}">See all({{ count((array) $page->version) }})</a>
 							@endif
@@ -44,17 +44,17 @@
 						@endif
 					</td>
 					<td>
-						<a target="_blank" class="btn btn-success" href="{{url($page->slug)}}" data-toggle="tooltip" data-placement="top" title="View page">
+						<a target="_blank" class="btn btn-success" style="color: white;" href="{{url($page->slug)}}" title="View page">
 							<i class="fa fa-eye"></i>
 						</a>
-						<a class="btn btn-primary" href="{{route('admin.page.update', ['id'=>$page->id])}}"  data-toggle="tooltip" data-placement="top" title="Edit page">
+						<a class="btn btn-primary" style="color: white;" href="{{route('admin.page.update', ['id'=>$page->id])}}" title="Edit page">
 							<i class="fa fa-pencil-square-o"></i>
 						</a>
 						@if(auth()->user()->id ==1 && !empty($versions) && count($versions) <= 1)
 						<form method="POST" action="{{ route('admin.page.delete' , ['id' => $page->id]) }}" accept-charset="UTF-8" style="display:inline" onsubmit="return confirm('This is last version of this page. Do you want to delete this page as a whole?');">
 							<input name="_method" type="hidden" value="DELETE">
 							<input name="_token" type="hidden" value="{{ csrf_token()}}">
-							<button type="submit" class="btn btn-danger confirm" data-confirm="Are you sure you want to delete this page?"  data-toggle="tooltip" data-placement="top" title="Delete page"><i class="fa fa-trash"> </i></button>
+							<button type="submit" class="btn btn-danger confirm" data-confirm="Are you sure you want to delete this page?" title="Delete page"><i class="fa fa-trash"> </i></button>
 						</form>
 						@endif
 					</td>
@@ -67,35 +67,35 @@
 							<td></td>
 							<td></td>
 							<td>
-								<div>
-									<span style="{{ $key == $page->version_no ? "font-weight: bolder":'' }}">v{{$key}}</span> <span class="badge">{{ \Carbon\Carbon::parse($versionContent->updated_at)->format('Y-m-d H:i:s') }}</span>
-								</div>
+								<span class="badge" style="{{ "font-weight: bolder; background-color: #337ab7;" }}">v{{$key}}</span>
+								@if ($key == $page->version_no) <span style="font-style: italic;">Current version</span> @endif
+								<span style="display: block;">{{ \Carbon\Carbon::parse($versionContent->updated_at)->format('M d,Y') }}</span>
 							</td>
 							<td>
-								<div>
+
 									@if ($key != $page->version_no)
 									<form method="POST" action="{{ route('admin.version.edit', ['id'=>$page->id]) }}" style="display: inline">
 										<input name="_token" type="hidden" value="{{ csrf_token()}}">
 										<input name="version_no" class="selected" type="hidden" value="{{ $key }}">
-										<button class="btn btn-success" type="submit"  data-toggle="tooltip" data-placement="top" title="Publish this version">
+										<button class="btn btn-success" style="margin-right: 4px;" type="submit" title="Publish this version">
 											<i class="fa fa-check"></i>
 										</button>
 									</form>
 									@endif
-									<a target="_blank" class="btn btn-success" href="{{url($page->slug)}}?v={{$key}}"  data-toggle="tooltip" data-placement="top" title="View this page version">
+									<a target="_blank" style="color:white;" class="btn btn-success" href="{{url($page->slug)}}?v={{$key}}" title="View this page version">
 										<i class="fa fa-eye"></i>
 									</a>
-									<a class="btn btn-primary" href="{{route('admin.page.update', ['id'=>$page->id])}}?v={{$key}}"  data-toggle="tooltip" data-placement="top" title="Edit this version">
+									<a class="btn btn-primary" style="color:white;" href="{{route('admin.page.update', ['id'=>$page->id])}}?v={{$key}}" title="Edit this version">
 										<i class="fa fa-pencil-square-o"></i>
 									</a>
 									@if(auth()->user()->id ==1 && $key != $page->version_no)
 										<form method="POST" action="{{ route('admin.page.version.delete' , ['id' => $page->id, 'version' => $key]) }}" accept-charset="UTF-8" style="display:inline" onsubmit="return confirm('Are you sure you want to delete this page?');">
 											<input name="_method" type="hidden" value="DELETE">
 											<input name="_token" type="hidden" value="{{ csrf_token()}}">
-											<button type="submit" class="btn btn-danger confirm" data-confirm="Are you sure you want to delete this page?"  data-toggle="tooltip" data-placement="top" title="Delete this version"><i class="fa fa-trash"> </i></button>
+											<button type="submit" class="btn btn-danger confirm" data-confirm="Are you sure you want to delete this page?" title="Delete this version"><i class="fa fa-trash"> </i></button>
 										</form>
 									@endif
-								</div>
+
 							</td>
 						</tr>
 					@endforeach
@@ -107,6 +107,9 @@
 </div>
 @stop
 @section('js')
+	<style>
+
+	</style>
 <script type="text/javascript">
 	$('document').ready(function() {
 		$('.see-versions').on('click', function(e) {
