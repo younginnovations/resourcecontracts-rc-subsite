ce8092216125e2ad18d8bd16ca42756fadeacd34
Sweta Shrestha <sweta.shrestha@yipl.com.np>
2020-07-13 13:15:17 +0545

    optimising css

--- resources/views/site/new-home.blade.php
+++ resources/views/site/new-home.blade.php
@@ -131,26 +131,7 @@ $currentLang = app('translator')->getLocale();
 	</script>
 	<script src="{{url('js/homepage.js')}}"></script>
 	<script>
-
-		if($('body').hasClass('lang-ar')) {
-			$('.annotation-wrap').slick({
-				dots: true,
-				arrows: false,
-				autoplay: true,
-				autoplaySpeed: 2400,
-				rtl: true
-			});
-		}
-
-		else {
-			$('.annotation-wrap').slick({
-				dots: true,
-				arrows: false,
-				autoplay: true,
-				autoplaySpeed: 2400,
-			});
-		}
-
+		$("body").hasClass("lang-ar")?$(".annotation-wrap").slick({dots:!0,arrows:!1,autoplay:!0,autoplaySpeed:2400,rtl:!0}):$(".annotation-wrap").slick({dots:!0,arrows:!1,autoplay:!0,autoplaySpeed:2400});
 	</script>
 @stop
 @endif
