c0525ecc0400e0d18b5448709d973482e829a1e2
Catalin Andrei <candrei@users.noreply.github.com>
2020-07-28 13:24:19 -0400

    Ensure that the protected hosts are not picked up as default

--- config/init.sh
+++ config/init.sh
@@ -81,8 +81,7 @@ export CONTACT_MAIL=${TZ_CONTACT_MAIL}
 export COUNTRY=tz
 
 export CATEGORY=country-tz
-envsubst '$SERVER_NAME $CATEGORY'< ./nginx_subsite_
-ted.template > /etc/nginx/sites-enabled/country-tz
+envsubst '$SERVER_NAME $CATEGORY'< ./nginx_subsite_protected.template > /etc/nginx/sites-enabled/protected-country-tz
 
 export CATEGORY=rc
 envsubst < ./env.template > /var/www/country-tz/.env
