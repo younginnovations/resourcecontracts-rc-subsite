36b488faf7348e320c3bc1212d6295b0520b02be
Manish Pradhan <manish.pradhan@yipl.com.np>
2020-08-04 14:05:03 +0545

    1307-guide-page-ui
    - [x] guide page blade added
    - [x] UI of guides page
    - [x] guide page rendered from admin side
    - [x] Uncommenting in search blade file
    - [x] Updated css version for new changes
    - [x] guide page blade added
    - [x] UI of guides page
    - [x] guide page rendered from admin side
    - [x] Uncommenting in search blade file
    - [x] Updated css version for new changes
    - [x] page route fixed
    - [x] Fixing spacing in sections
    - [x] Changed page footer
    - [x] UI of footer
    - [x] Added new layout for footer layout
    - [x] footer fixes
    - [x] Added new line to guides.blade.php
    - [x] guide nav color fix
    - [x] gudie page for OLC only
    - [x] Consistency of static pages
    - [x] Source Code Structure Formatting
    - [x] hamburger order updated, footer order updated, prev and next link added in other pages
    - [x] side bar updated in all pages for olc
    - [x] UI Improvements of guide page
    - [x] added download button for pdf
    - [x] Added PDF Download
    - [x] Added Spipu-html-to-pdf
    - [x] spipu html2pdf, css styles unsupported
    - [x] WIP guide page pdf ui
    - [x] pdf download button shifted down
    - [x] added id to tags in guide page
    - [x] rebased and composer updated
    - [x] setting font for pdf
    - [x] fixing font size and spacing for pdf
    - [x] Fixing pdf layout
    - [x] php gd extension added
    - [x] fonts added from public to vendor via docker

--- resources/views/layout/partials/new-sidebar.blade.php
+++ resources/views/layout/partials/new-sidebar.blade.php
@@ -15,13 +15,13 @@
 			<li><a href="{{url('contact')}}" @if(isActiveMenu('contact')) class="active" @endif>@lang('sidebar.contact')</a></li>
 		@endif
 		@if(site()->isOLC())
-			<li><a href="{{url('/')}}" @if(isActiveMenu('')) class="active" @endif> @lang('sidebar.home') </a></li>
-			<li><a href="{{url('countries')}}" @if(isActiveMenu('countries')) class="active"@endif>@lang('sidebar.browse_by_country')</a>
-			<li><a href="{{url('resources')}}" @if(isActiveMenu('resources')) class="active"@endif>@lang('sidebar.browse_by_resource')</a></li>
-			<li><a href="{{url('about')}}" @if(isActiveMenu('about')) class="active" @endif>@lang('sidebar.aboutOLC')</a></li>
-			<li><a href="{{url('guides')}}" @if(isActiveMenu('guides')) class="active" @endif>@lang('sidebar.guidesOLC')</a></li>
-			<li><a href="{{url('glossary')}}" @if(isActiveMenu('glossary')) class="active"@endif>@lang('sidebar.glossary')</a></li>
-			<li><a href="{{url('faqs')}}" @if(isActiveMenu('faqs')) class="active" @endif>@lang('sidebar.faqs')</a></li>
+			<li><a href="{{url('/')}}" @if(isActiveMenu('')) class="active" @endif > @lang('sidebar_olc.home') </a></li>
+			<li><a href="{{url('countries')}}" @if(isActiveMenu('countries')) class="active" @endif>@lang('sidebar_olc.view_by_country')</a></li>
+			<li><a href="{{url('resources')}}" @if(isActiveMenu('resources')) class="active" @endif>@lang('sidebar_olc.view_by_resource')</a></li>
+			<li><a href="{{url('about')}}" @if(isActiveMenu('about')) class="active" @endif>@lang('sidebar_olc.about')</a></li>
+			<li><a href="{{url('guides')}}" @if(isActiveMenu('guides')) class="active" @endif>@lang('sidebar_olc.use_this_site')</a></li>
+			<li><a href="{{url('glossary')}}" @if(isActiveMenu('glossary')) class="active" @endif>@lang('sidebar_olc.glossary')</a></li>
+			<li><a href="{{url('faqs')}}" @if(isActiveMenu('faqs')) class="active" @endif>@lang('sidebar_olc.faqs')</a></li>
 		@endif
 		@if(site()->isCountrySite())
 			<li><a href="{{url('/')}}" @if(isActiveMenu('')) class="active" @endif> @lang('sidebar.home') </a></li>
