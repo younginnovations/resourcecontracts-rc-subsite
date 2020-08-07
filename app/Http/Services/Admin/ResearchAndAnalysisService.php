a2e7c1204116618da8fae092da064fae5c457fd3
Birendra Gurung <birendragurung007@gmail.com>
2020-08-07 12:03:57 +0545

    Refactoring codes & docblocks

--- app/Http/Services/Admin/ResearchAndAnalysisService.php
+++ app/Http/Services/Admin/ResearchAndAnalysisService.php
@@ -6,6 +6,9 @@ namespace App\Http\Services\Admin;
 
 use App\Http\Models\ResearchAndAnalysis\ResearchAndAnalysis;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class ResearchAndAnalysisService
 {
@@ -23,44 +26,76 @@ class ResearchAndAnalysisService
         $this->researchAndAnalysis = $researchAndAnalysis;
     }
 
+    /**
+     * @return ResearchAndAnalysis[]|\Illuminate\Database\Eloquent\Collection
+     */
     public function all()
     {
         return $this->researchAndAnalysis->all();
     }
 
+    /**
+     * @return mixed
+     */
     public function paginate()
     {
         return $this->researchAndAnalysis->whereNull('featured_index')->orderBy('updated_at','desc')->paginate();
     }
 
+    /**
+     * @param array $attributes
+     * @return ResearchAndAnalysis
+     */
     public function create(array $attributes)
     {
         $attributes['status'] = 0;
         return $this->researchAndAnalysis->create($attributes);
     }
 
+    /**
+     * @param $id
+     * @param array $data
+     * @return mixed
+     */
     public function update($id, array $data)
     {
         $research = $this->researchAndAnalysis->find($id);
         return $research->update($data);
     }
 
+    /**
+     * @param $id
+     * @return ResearchAndAnalysis
+     *
+     * @throws ModelNotFoundException
+     */
     public function find($id)
     {
         return $this->researchAndAnalysis->findOrFail($id);
     }
 
+    /**
+     * @param $id
+     * @return bool
+     */
     public function delete($id)
     {
         $research = $this->researchAndAnalysis->find($id);
         return $research->delete();
     }
 
+    /**
+     * @return Collection
+     */
     public function getFeatured()
     {
         return $this->researchAndAnalysis->whereNotNull('featured_at')->orderBy('featured_index')->limit(3)->get();
     }
 
+    /**
+     * @param array $featuredAttributes
+     * @return Collection
+     */
     public function updateFeatured(array $featuredAttributes)
     {
         $ids = array_pluck($featuredAttributes, 'id');
