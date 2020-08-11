3b57938aefa0985aec18b940ab0941cab81c9531
Birendra Gurung <birendragurung007@gmail.com>
2020-08-11 17:26:06 +0545

    Added new layout for footer layout

--- resources/views/layout/app-page.blade.php
+++ resources/views/layout/app-page.blade.php
@@ -1,5 +1,5 @@
 @include('layout.partials.head')
 @include('layout.partials.header')
 @include('layout.partials.sidebar')
-    @yield('content')
-@include('layout.partials.footer')
+@yield('content')
+@include('layout.partials.new-footer')
