5cbbe4fde73ab494bde9edcc3bb8af142df9e9f3
Manish Pradhan <manish.pradhan@yipl.com.np>
2020-12-08 14:09:54 +0545

    codes formatted

--- resources/views/layout/partials/new-sidebar.blade.php
+++ resources/views/layout/partials/new-sidebar.blade.php
@@ -8,19 +8,19 @@
 		<li><a href="{{url('about')}}" @if(isActiveMenu('about')) class="active" @endif>@lang('sidebar.about')</a>
 
 		<li><a href="{{url('faqs')}}" @if(isActiveMenu('faqs')) class="active" @endif>@lang('sidebar.faqs')</a></li>
-		
+
 		<li><a href="{{url('guides')}}" @if(isActiveMenu('guides')) class="active" @endif>@lang('sidebar.guides')</a>
 		</li>
 		<li><a href="{{url('glossary')}}"
 			   @if(isActiveMenu('glossary')) class="active" @endif>@lang('sidebar.glossary')</a></li>
-		
-			   @if(site()->isRC())
-					<li><a href="{{url('research-and-analysis')}}">@lang('sidebar.research_and_analysis')</a></li>
-				@endif
 
-				@if(!site()->isCountrySite())
-					<li><a href="{{url('country-sites')}}">@lang('sidebar.country_sites')</a></li>
-				@endif
-				<li><a href="{{url('contact')}}">@lang('sidebar.contact')</a></li>
+		@if(site()->isRC())
+			<li><a href="{{url('research-and-analysis')}}">@lang('sidebar.research_and_analysis')</a></li>
+		@endif
+
+		@if(!site()->isCountrySite())
+			<li><a href="{{url('country-sites')}}">@lang('sidebar.country_sites')</a></li>
+		@endif
+		<li><a href="{{url('contact')}}">@lang('sidebar.contact')</a></li>
 	</ul>
 </div>
