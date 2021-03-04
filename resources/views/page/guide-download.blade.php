f0af3ab8d8fbf5ebfd35dbc31915d401052fa4bd
Sweta Shrestha <sweta.shrestha@yipl.com.np>
2021-03-04 13:45:44 +0545

    Working on feedbacks of guide pdf page

--- resources/views/page/guide-download.blade.php
+++ resources/views/page/guide-download.blade.php
@@ -5,14 +5,8 @@
         color: #404040;
     }
 
-    .logo {
-        font-size: 19px;
-        margin-top: -50px;
-    }
-
     h1 {
-        font-size: 38px;
-        margin-top: 50px;
+        font-size: 32px;
         margin-bottom: 30px;
     }
 
@@ -45,32 +39,44 @@
    }
 </style>
 
-<page backtop="60px" backbottom="40px" backleft="40px" backright="40px">
-   <page_header>
-        <table class="page_header" width="100%"> 
-            <tr>
-                <td width="560"></td>
-                <td width="200" style="vertical-align: middle; text-align: right; font-size: 12px;"><em>Page [[page_cu]] of [[page_nb]]</em></td>
+<page backtop="40px" backbottom="30px" backleft="40px" backright="40px">
+   <page_footer>
+        <table class="page_footer" width="100%">
+            <tr>   
+                <td style="font-size: 12px; font-family: OpenSans; line-height: 18px; padding-left: 40px; padding-bottom: 30px; ">
+                     <p style="padding-right: 30px; color: #4a4a4a;">OpenLandContracts is an online repository of publicly available contracts for agriculture, forestry, and other land- based
+                    investment projects. To learn more and view or download documents, go to openlandcontracts.org.</p>
+                </td>
             </tr>
         </table>
-   </page_header>  
-    <strong class="logo"><span style="color: #27ae60;">OPENLAND</span> <br> <span style="color: rgba(64,64,64,0.7)">CONTRACTS</span></strong> 
-    <h1>How to use OpenLand Contracts?</h1>
-    <h2>Introduction</h2>
-    <p>ResourceContracts temporary is a repository of publicly available oil, gas, and mining contracts. The repository features plain language summaries of each contract’s key social, environmental, human rights, fiscal, and operational terms, and tools for searching and comparing contracts. ResourceContracts.org promotes greater transparency of investments in the extractive industries, and facilitates a better understanding of the contracts that govern them. Contracts that govern the relationship between governments and oil, gas, and mining companies in the extraction and exploitation of natural resources are complex. The related licenses, leases, concessions, and other contractual arrangements affect everything from tax liabilities to local content obligations to the protection of the environment. Despite the critical role these contracts play in setting the rules for investments in extractive industries, they are often difficult to discover. Even when publicly available, the tools to assess and compare the key provisions of such contracts are limited. This can result in a critical lack of knowledge for governments as they try to negotiate the best terms for their citizens, and can result in missed opportunities to learn from others’ past successes or missteps.</p>
+        <table>
+            <tr>
+                <td width="570" style="font-size: 12px; font-family: OpenSans; padding-left: 40px; color: #4a4a4a; padding-bottom: 0;">OpenLandContracts - How to Guides</td>
+                <td width="100" style="text-align: right; font-size: 12px; font-family: OpenSans; color: #4a4a4a; padding-bottom: 0;"><em>Page [[page_cu]] of [[page_nb]]</em></td>
+            </tr>
+        </table>
+   </page_footer>
+    <table width="100%"> 
+        <tr> 
+            <td width="500"><img src="images/logo_olc.png" alt="CCSI" width="120"></td>
+            <td width="200"><img src="images/logo_ccsi.png" alt="CCSI" width="180" class="ccsi-logo"></td>
+        </tr>
+    </table>
+    <h1>OpenLand Contracts - How to Guides</h1>
+    <p>OpenLandContracts is an online repository of publicly available contracts and related documents for agriculture, forestry, and other land-based investment projects. The site was created by the Columbia Center on Sustainable Investment (CCSI) and its partners to promote greater transparency of land-based investments and facilitate a better understanding of the contracts that govern them.</p>
+    <p>To search, view and download documents in the repository and to learn more about using the site, visit openlandcontracts.org.</p>
     <h2>How to Guides</h2>
     <table width="100%">
         <tr>
-            <td width="240" style="padding-right: 20px;"><img src="images/guide-step-1.png" alt="image" width='260' height='170'></td>
+            <td width="240" style="padding-right: 20px;"><img src="images/bycountry.jpg" alt="By Country" width='260' height='170'></td>
             <td width="420">
                 <h3>Find Documents by Country or Resource</h3>
                 <ul>
-                    <li>To see any contract, click on the contract title to go to the View Document page.</li>
-                    <li>You can scroll through the full PDF document page by page, or click the &ldquo;TEXT&rdquo; button to switch to a text- only view.</li>
-                    <li>To search for specific words in the text, use the &ldquo;Search in this document&rdquo; box on the left. Note that text versions are created automatically and may contain errors.</li>
-                    <li>OpenLandContracts.org features Annotations (i.e. plain-language summaries) on the left of the page that explain key provisions throughout the document. See more about Annotations below.</li>
-                    <li>To download or share the document, use the buttons above the right column.</li>
-                    <li>To view key document details such as source website, Associated Documents and metadata, switch to the Summary Page using the green button on the right.</li>
+                    <li>OpenLandContracts offers quick access to documents from a specific country or related to a specific resource (e.g., timber, palm oil). Use the global <strong>sidebar menu</strong> or the world map on the homepage to view by Country or by Resource.</li>
+                    <li>On each Country page, you can <strong>sort</strong> documents by title, by year of signature, by resource, or by document type. In the right column, you’ll also find links to view country documents for specific resources. (Each Resource page has the same features.)</li>
+                    <li>Use the search box <strong>immediately above the results</strong> to narrow your search using specific words or phrases.</li> 
+                    <li>You can download country lists, resource lists, and search results using the <strong>Download button</strong> above the list on the right. The downloaded spreadsheet will contain the title, details, and web address (URL) for every contract and document displayed. </li>
+                    <li>These pages to view by Country or Resource are available for easy browsing, but you can also use the main Search Box to search on the same terms.</li>
                 </ul>
             </td>
         </tr>
@@ -80,31 +86,40 @@
             <td width="420">
                 <h3>Search All Documents</h3>
                 <ul>
-                    <li>To search OpenLandContracts, enter any word or phrase in the <strong>Search Box</strong> at the top of most pages.</li>
-                    <li>To narrow your search, use the advanced <strong>Search Tools</strong> to limit the results shown. Select from the options in each category (Country, Resource, Year Signed, etc.) and click the green Search button to narrow your search. You can <strong>select multiple categories</strong>&mdash;and multiple options within each category&mdash;to further narrow the results.</li>
+                    <li>To search OpenLandContracts, enter any word or phrase in the <strong>Search Box</strong> at the top of most pages.</li> 
+                    <li>To narrow your search, use the advanced <strong>Search Tools</strong> to limit the results shown. Select from the options in each category (Country, Resource, Year Signed, etc.) and click the green Search button to narrow your search. You can <strong>select multiple categories—</strong>and multiple options within each category—to further narrow the results.</li>
                     <li>To show only <strong>recent documents</strong> (past 90 days) or only contracts with <strong>Annotations</strong>, use the checkboxes in the Search Tools. See more about Annotations below.</li>
+                </ul>
+            </td>
+            <td width="240" style="padding-left: 20px; padding-bottom: 0;"><img src="images/search.jpg" alt="Search" width='260' height='170'></td>
+        </tr>
+    </table>
+    <table>
+        <tr>
+            <td width="240"><img src="images/search2.jpg" alt="Search" width='260' height='170'></td>
+            <td width="420" style="padding-left: 20px;">
+                <ul>
                     <li>Use the gray <strong>Reset</strong> button to remove all previous search criteria and start a new search.</li>
-                    <li>You can <strong>share or download</strong> the results of any search using the buttons on the right above your list of results.</li>
+                    <li>You can <strong>share or download</strong> the results of any search using the buttons on the right above your list of results.</li> 
                     <li>Search results are based on matching terms in the text, the title or the Annotations of documents on OpenLandContracts.</li>
                     <li>Searching for contracts using an Annotation Category will allow you to view all contracts that include the type of Annotation you selected (e.g., Community Consultation, Resettlement, Water Use, etc.).</li>
                 </ul>
             </td>
-            <td width="240" style="padding-left: 20px;"><img src="images/guide-step-2.png" alt="image" width='260' height='170'></td>
         </tr>
     </table>
 
    <table width="100%">
         <tr>
-            <td width="240" style="padding-right: 20px;"><img src="images/guide-step-3.png" alt="image" width='260' height='170'></td>
+            <td width="240" style="padding-right: 20px;"><img src="images/viewdoc.jpg" alt="View Document" width='260' height='170'></td>
             <td width="420">
                 <h3>View a Document</h3>
                 <ul>
-                    <li>To see any contract, click on the contract title to go to the View Document page.</li>
-                    <li>You can scroll through the full PDF document page by page, or click the &ldquo;TEXT&rdquo; button to switch to a text- only view.</li>
-                    <li>To search for specific words in the text, use the &ldquo;Search in this document&rdquo; box on the left. Note that text versions are created automatically and may contain errors.</li>
-                    <li>OpenLandContracts.org features Annotations (i.e. plain-language summaries) on the left of the page that explain key provisions throughout the document. See more about Annotations below.</li>
-                    <li>To download or share the document, use the buttons above the right column.</li>
-                    <li>To view key document details such as source website, Associated Documents and metadata, switch to the Summary Page using the green button on the right.</li>
+                    <li>To view a specific document, click on the <strong>document title</strong> to go to the View Document page.</li>
+                    <li>You can scroll through <strong>the full PDF document</strong> page by page, or click the <strong> &ldquo;TEXT&rdquo; button  </strong>to switch to a text- only view.</li>
+                    <li>To search for specific words in the text, use the  <strong> &ldquo;Search in this document&rdquo;  </strong> box on the left. Note that text versions are created automatically and may contain errors.</li>
+                    <li>OpenLandContracts.org features <strong>Annotations </strong> (i.e. plain-language summaries) on the left of the page that explain key provisions throughout the document.</li>
+                    <li>To  <strong>download or share </strong> the document, use the buttons above the right column.</li>
+                    <li>To view  <strong>key document details </strong> such as Associated Documents, the original source website, or document metadata, switch to the Summary Page using <strong>the green button on the top right of the page</strong>.</li>
                 </ul>
             </td>
         </tr>
@@ -113,31 +128,49 @@
     <table width="100%">
         <tr>
             <td width="420">
-                <h3>View Annotations</h3>
+                <h3>View Annotations of Contract Terms</h3>
                 <ul>
-                    <li>To see any contract, click on the contract title to go to the View Document page.</li>
-                    <li>You can scroll through the full PDF document page by page, or click the &ldquo;TEXT&rdquo; button to switch to a text- only view.</li>
-                    <li>To search for specific words in the text, use the &ldquo;Search in this document&rdquo; box on the left. Note that text versions are created automatically and may contain errors.</li>
+                    <li>OpenLandContracts features hundreds of contract <strong>Annotations</strong>: plain-language summaries that help to explain key provisions and to locate certain topics within a contract.</li>
+                    <li>Annotations are listed <strong>alongside a contract</strong> on the left as you view the document, and marked with yellow boxes on relevant pages as you scroll through the contract PDF. Click on an Annotation to skip to the relevant page in the PDF viewer.</li>
+                    <li>Annotations are organized by the Topic of the related contract provision. Topics include: <strong>Environment, Fiscal, Legal Rules, Social, Operations and General</strong>. You can view a contract's Annotations by Topic or in order by page. </li> 
+                    <li>Every Annotation has its own <strong>unique web address (URL)</strong>, to make it easier to bookmark, cite, and share specific contract provisions. </li>
+                    <li>Please note that Annotations offer short summaries that explain contract terms. <strong>They do not interpret or analyze the terms</strong> or the overall contract. For more about contract analysis and our methodology, see the FAQ page.</li>
                 </ul>
             </td>
-            <td width="240" style="padding-left: 20px"><img src="images/guide-step-4.png" alt="image" width='260' height='170'></td>
+            <td width="240" style="padding-left: 20px"><img src="images/anno.jpg" alt="image" width='260' height='170'></td>
         </tr>
     </table>
 
     <table width="100%">
         <tr>
-            <td width="240" style="padding-right: 20px;"><img src="images/guide-step-3.png" alt="image" width='260' height='170'></td>
+            <td width="240" style="padding-right: 20px;"><img src="images/allclips.jpg" alt="View Document" width='260' height='170'></td>
             <td width="420">
-                <h3>Download and Share</h3>
+                <h3>Compare Using Clips Tool</h3>
                 <ul>
-                    <li>To search OpenLandContracts.org, enter any word or phrase in the Search Box at the top of most pages.</li>
-                    <li>To narrow your search, use the Advanced Search Tools to limit the results shown. Select from the options in each category (Country, Resource, Year Signed, etc.) and click the green Search button to narrow your search. You can select multiple categories&mdash;and multiple options within each category&mdash;to further limit the results.</li>
-                    <li>To show only recent documents (past 90 days) or only contracts containing Annotations, use the checkboxes in the Search Tools area. See more about Annotations below.</li>
-                    <li>You can share the results of any search using the Share button above your results on the right.</li>
-                    <li>Use the gray Reset button to remove all previous search criteria and start a new search.</li>
-                    <li>Search results are based on matching terms in the text, title or Annotations of documents on OpenLandContracts.org.</li>
+                    <li>You can <strong>compare the text of terms in different contracts</strong>, by using the <strong>Clips tool</strong> to select, view, or download multiple Annotations from multiple contracts.</li>
+                    <li>Click the [icon] paper clip button at the top right of any contract page to turn on the Clips function, then <strong>select any Annotation</strong> to add it to your saved Clips. You can <strong>continue adding Clips from any document</strong> as long as the Clips tool is on.</li>   
+                    <li>To review your selected Clips, click the blue “View Clips” button to go to the <strong>All Clips</strong> page. You can sort and share this list of Clips like any other OpenLandContracts list.</li>
+                    <li>To make it easier to compare terms, the <strong>All Clips</strong> page also offers enhanced print and download bundles. Select <strong>Print</strong> to create a document with the full text of all the Annotations in your Clips. Select <strong>Download</strong> to save a file including the document with the full Annotation texts, a spreadsheet with text, metadata and URLs for each Annotation, and a PDF that compiles the contract pages that correspond to each Annotation you clipped.</li>
+                    <li>To <strong>quickly compare a small number of Annotations</strong> on your screen, you can also use the red [icon] View buttons in the last column of the All Clips page. Click the buttons to display each relevant contract page as a pop-up window. If you want to compare more than four or five contract provisions, we recommend using the Download button on the All Clips page instead.</li>
                 </ul>
             </td>
+        </tr>
+    </table>
+
+    <table width="100%">
+        <tr>
+            <td width="420">
+                <h3>Download or Share</h3>
+                <ul>
+                    <li>OpenLandContracts provides multiple ways to <strong>save and share</strong> documents, Annotations, and search results.</li>
+                    <li>Every contract or document in the repository has a [icon] <strong>Download button</strong> near the top right of the View Document page. You can also download each document in lists or search results pages.</li>
+                    <li>To share a document via email, Twitter or Facebook, use the [icon] <strong>Share button</strong> near the top right of the View Document page. </li>
+                    <li>To <strong>share any list of documents or search results</strong>, use the Share button above the list on the right.</li>
+                    <li>In addition to sharing any list of documents or search results, you can <strong>download the full list</strong> or results as a spreadsheet containing the title, details and web address for every document displayed. </li>
+                    <li>When you save Annotations with the <strong>Clips</strong> tool, you can share, download or print your saved Clips, including options for a printout or spreadsheet containing the full text of all your clipped Annotations.</li>
+                </ul>
+            </td>
+            <td width="240" style="padding-right: 20px;"><img src="images/share.jpg" alt="image" width='260' height='170'></td>
         </tr>
     </table>
 </page>
